47d921b18db0a1011b52229deace8396fb55fe80
David McEwan <dmcewa15@caledonian.ac.uk>
2014-07-17 22:07:42 +0100

    Cherry keystems tested parameters and arrayed for easier production.

--- cherrymx_cross.py
+++ cherrymx_cross.py
@@ -51,15 +51,70 @@ def cherrymx_keycross_points(
     return pts
 # }}}
 
-def cherrymx_keycross_profile(
-                     height=4.0,
-                     width=4.0,
-                     height_thk=1.1,
-                     width_thk=1.3,
-                     depth=3.0,
+def cherrymx_keysup_points(
+                    height=5.2,
+                    width=6.8,
+                    endmill=1.0,
+                   ): # {{{
+    '''Generate points for polygon used for making CherryMX cross support for keycaps.
+    '''
+
+    assert isinstance(width, float) and width > 0.0
+    
+    # Calculate points to move to.
+    y = height/2 + endmill/2
+    x = width/2 + endmill/2
+    
+    # Points relative to centre listed in CW direction.
+    pts = [
+           (-x, +y),
+           (+x, +y),
+           (+x, -y),
+           (-x, -y),
+          ]
+    
+    return pts
+# }}}
+
+def array_keystem_points(
+                    height=6,
+                    width=14,
+                    supheight=5.2,
+                    supwidth=6.8,
+                    space=1.0,
+                    endmill=1.0,
+                   ): # {{{
+    '''Generate points for polygon used for making CherryMX cross support for keycaps.
+    '''
+    
+    # Calculate points to move to.
+    xdiff = supwidth + endmill + space + endmill
+    ydiff = supheight + endmill + space + endmill
+    
+    # Move along X row, then up Y column to next row, then back along row.
+    pts = []
+    yacc = 0.0
+    for row in range(height):
+        r = [(col*xdiff, yacc) for col in range(width)]
+        if not row % 2:
+            r.reverse()
+        yacc += ydiff
+        pts += r
+    
+    return pts
+# }}}
+
+def cherrymx_keystem_profile(
+                     crossheight=4.5,
+                     crosswidth=4.5,
+                     crossheight_thk=1.1,
+                     crosswidth_thk=1.3,
+                     supheight=5.2,
+                     supwidth=6.8,
+                     depth=3.3,
                      pitch=1.0,
-                     feedrate=660.0,
-                     plungerate=500.0,
+                     feedrate=100.0,
+                     plungerate=100.0,
                      clearance=5.0,
                      endmill=1.0,
                      direction='ccw',
@@ -68,8 +123,8 @@ def cherrymx_keycross_profile(
     '''Generate gcode for a hole at the current position to hold cherry mx keystem.
     '''
 
-    assert isinstance(width, float) and width > 0.0
-    assert isinstance(height, float) and height > 0.0
+    assert isinstance(crosswidth, float) and crosswidth > 0.0
+    assert isinstance(crossheight, float) and crossheight > 0.0
     assert isinstance(depth, float) and depth > 0.0
     assert isinstance(pitch, float) and pitch > 0.0
     assert isinstance(feedrate, float) and feedrate > 0.0
@@ -77,22 +132,35 @@ def cherrymx_keycross_profile(
     assert isinstance(clearance, float) and clearance > 0.0
     # Endmill size > 0 assertion not needed as you may want to just generate a
     #   "dumb" path for a different shape of endmill.
-    assert isinstance(endmill, float) and endmill < width
+    assert isinstance(endmill, float) and endmill < crosswidth
     assert isinstance(direction, str) and direction in ['cw', 'ccw']
     assert isinstance(ablpd, bool)
     
-    pts = cherrymx_keycross_points(height, width, height_thk, width_thk, endmill)
+    pts = cherrymx_keycross_points(crossheight,
+                                   crosswidth,
+                                   crossheight_thk,
+                                   crosswidth_thk,
+                                   endmill)
     
     # Initialise gcode lines.
     g = []
-    
-    # Set units as millimeters.
-    g.append('G21')
     
     # Use relative positioning (as opposed to absolute).
     # Required to make this code callable like a function.
     g.append('G91')
     
+    g.append(polygon_profile(
+                             pts=pts,
+                             depth=depth,
+                             pitch=pitch,
+                             feedrate=feedrate,
+                             plungerate=plungerate,
+                             clearance=clearance,
+                             ablpd=ablpd,
+                            ))
+    
+    # Now the surrounding support square.
+    pts = cherrymx_keysup_points(supheight, supwidth, endmill)
     g.append(polygon_profile(
                              pts=pts,
                              depth=depth,
@@ -112,45 +180,75 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--height',
+    parser.add_argument('--arrayheight',
                         action='store',
-                        default=4.0,
-                        type=float,
-                        help='cross height')
+                        default=6,
+                        type=int,
+                        help='array height (number)')
     
-    parser.add_argument('--width',
+    parser.add_argument('--arraywidth',
                         action='store',
-                        default=4.0,
-                        type=float,
-                        help='cross width')
+                        default=14,
+                        type=int,
+                        help='array width (number)')
     
-    parser.add_argument('--height_thk',
+    parser.add_argument('--arrayspace',
                         action='store',
-                        default=1.1,
+                        default=1.0,
                         type=float,
-                        help='Thickness of up/down spars')
+                        help='array spacing (mm)')
     
-    parser.add_argument('--width_thk',
+    parser.add_argument('--crossheight',
                         action='store',
-                        default=1.3,
+                        default=4.5,
                         type=float,
-                        help='Thickness of left/right spars')
+                        help='cross height (mm)')
+    
+    parser.add_argument('--crosswidth',
+                        action='store',
+                        default=4.5,
+                        type=float,
+                        help='cross width (mm)')
+    
+    parser.add_argument('--crossheight_thk',
+                        action='store',
+                        default=1.05,
+                        type=float,
+                        help='Thickness of up/down spars (mm)')
+    
+    parser.add_argument('--crosswidth_thk',
+                        action='store',
+                        default=1.2,
+                        type=float,
+                        help='Thickness of left/right spars (mm)')
+    
+    parser.add_argument('--supheight',
+                        action='store',
+                        default=5.2,
+                        type=float,
+                        help='support square height (mm)')
+    
+    parser.add_argument('--supwidth',
+                        action='store',
+                        default=6.8,
+                        type=float,
+                        help='support square width (mm)')
     
     parser.add_argument('--depth',
                         action='store',
-                        default=3.0,
+                        default=3.5,
                         type=float,
-                        help='cross depth')
+                        help='cross depth (mm)')
     
     parser.add_argument('--pitch',
                         action='store',
                         default=1.0,
                         type=float,
-                        help='cutting pitch')
+                        help='cutting pitch (mm)')
     
     parser.add_argument('--feedrate',
                         action='store',
-                        default=100.0,
+                        default=200.0,
                         type=float,
                         help='feedrate (mm/minute)')
     
@@ -180,7 +278,7 @@ if __name__ == '__main__':
     
     parser.add_argument('--ablpd',
                         action='store',
-                        default=1,
+                        default=0,
                         type=int,
                         choices=[0, 1],
                         help='Anti Backlash Point Drilling')
@@ -197,24 +295,38 @@ if __name__ == '__main__':
     g.append('G21')
     
     # Assume spindle starts at zero.
-    g.append('G91 G0 Z%s' % floatf(args.clearance))
+    g.append('G0 Z%s' % floatf(args.clearance))
     
-    # Cherry profile function should assume spindle at clearance.
-    g.append(cherrymx_keycross_profile(
-                              height=args.height,
-                              width=args.width,
-                              height_thk=args.height_thk,
-                              width_thk=args.width_thk,
-                              depth=args.depth,
-                              pitch=args.pitch,
-                              feedrate=args.feedrate,
-                              plungerate=args.plungerate,
-                              clearance=args.clearance,
-                              endmill=args.endmill,
-                              direction=args.direction,
-                              ablpd=bool(args.ablpd),
-                             ))
+    array_pts = array_keystem_points(
+                                     height=args.arrayheight,
+                                     width=args.arraywidth,
+                                     supheight=args.supheight,
+                                     supwidth=args.supwidth,
+                                     space=args.arrayspace,
+                                     endmill=args.endmill,
+                                    )
+    for pt in array_pts:
+        g.append('G0 X%s Y%s' % (floatf(pt[0]), floatf(pt[1])))
+        # Cherry profile function should assume spindle at clearance.
+        g.append(cherrymx_keystem_profile(
+                                          crossheight=args.crossheight,
+                                          crosswidth=args.crosswidth,
+                                          crossheight_thk=args.crossheight_thk,
+                                          crosswidth_thk=args.crosswidth_thk,
+                                          supheight=args.supheight,
+                                          supwidth=args.supwidth,
+                                          depth=args.depth,
+                                          pitch=args.pitch,
+                                          feedrate=args.feedrate,
+                                          plungerate=args.plungerate,
+                                          clearance=args.clearance,
+                                          endmill=args.endmill,
+                                          direction=args.direction,
+                                          ablpd=bool(args.ablpd),
+                                         ))
     # Cherry profile function should leave spindle at clearance.
+        g.append('G90')
+
 
     # Put gcode onto STDOUT to let caller do any file redirect.
     print('\n'.join(g))
