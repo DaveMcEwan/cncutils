72f23f705eedb1beadc132a3a087347eea4070f8
David McEwan <dmcewa15@caledonian.ac.uk>
2014-07-30 00:48:02 +0100

    Initial additions/changes for generating mcdox base plate too.

--- mcdox.py
+++ mcdox.py
@@ -3,9 +3,11 @@
 # All parts specified for LHS.
 # RHS should just be a reflection around X=0.
 
-print_stats = 0
-plot = 0
-print_gcode = 1
+swmnt_stats = 0
+swmnt_plot = 0
+swmnt_gcode = 0
+base_plot = 1
+base_gcode = 1
 
 from math import *
 import sys
@@ -136,13 +138,17 @@ center = (0.0, 0.0)
 # A4 dimensions are 297x210 so to fit nicely on cheap sheets of acrylic try to
 #   keep dimensions down.
 
+# TODO: In future versions it should be the base holes which have the rotate of
+#   11 degrees, although negative.
+# This hasn't been changed yet as I've already cut a pair of swmnt plates and
+#   need a base plate to match them.
 n_fix = 6
-fix_holes = gen_polygon_pts(n_fix, [radius-0.5*spc])
-fix_holes = pts_rotate(fix_holes, [3*2*pi/n_fix], center)
-fix_holes = pts_rotate(fix_holes, [radians(11)], center)
+base_holes = gen_polygon_pts(n_fix, [radius-0.5*spc])
+base_holes = pts_rotate(base_holes, [3*2*pi/n_fix], center)
+fix_holes = pts_rotate(base_holes, [radians(11)], center)
 
 # mx_holes is now a list of tuples containing the coordinates and rotations of all switches on LHS.
-if print_stats:
+if swmnt_stats:
     out = []
     out += ['Operations:']
     out += ['\tCherryMX holes:']
@@ -160,9 +166,8 @@ if print_stats:
 
 # TODO: Calculate LED holes.
 # TODO: Plot LED holes.
-# TODO: Generate gcode.
 
-if plot:
+if swmnt_plot:
     import matplotlib.pyplot as plt
     import matplotlib.patches as mpatches
     
@@ -191,7 +196,7 @@ if plot:
         ax.plot(x, y, color='g')
     plt.show()
 
-if print_gcode:
+if swmnt_gcode:
     from gcode_base import *
     from gcode_profile_circle import *
     from cherrymx_hole import *
@@ -237,4 +242,85 @@ if print_gcode:
                                 feedrate=feedrate,
                                ))
     
-    print('\n'.join(g))
+    with open('mcdox_swmnt.nc', 'w') as fd:
+        fd.write('\n'.join(g))
+
+
+# The base is composed of 2 circles with a thinner section in the middle, made
+#   from the arcs of other circles.
+# Centers of circles are:
+# A - left hand
+# B - bottom arc
+# C - right hand
+# D - top arc
+# Start stop points of the arcs are:
+# E - Between A and D
+# F - Between A and B
+# G - Between C and D
+# H - Between C and B
+# The size of the arcs is controlled by 3 parameters:
+# hand_sep - separation between hand plates
+# r_top - radius of top arc
+# r_bot - radius of bottom arc
+r_hand = radius
+hand_sep = 20.0
+r_top = sqrt(2)*r_hand
+r_bot = r_hand/sqrt(2)/sqrt(2)
+
+sep = hand_sep + 2*r_hand
+baseA = (r_hand, r_hand)
+baseB = (baseA[0] + sep/2, baseA[1] - sqrt((r_hand + r_bot)**2 - (sep/2)**2))
+baseC = (baseA[0] + sep, baseA[1])
+baseD = (baseB[0], baseA[1] + sqrt((r_hand + r_top)**2 - (sep/2)**2))
+baseE = pt_between_pts(baseA, baseD, r_hand/(r_hand+r_top))
+baseF = pt_between_pts(baseA, baseB, r_hand/(r_hand+r_bot))
+baseG = pt_between_pts(baseC, baseD, r_hand/(r_hand+r_top))
+baseH = pt_between_pts(baseC, baseB, r_hand/(r_hand+r_bot))
+
+if base_plot:
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as mpatches
+    
+    ax = plt.subplot(111, aspect=1)
+    ax.scatter(baseA[0], baseA[1])
+    a = mpatches.Circle(baseA, r_hand, fill=False)
+    ax.add_patch(a)
+    
+    ax.scatter(baseB[0], baseB[1])
+    b = mpatches.Arc(
+                     xy=baseB,
+                     width=2*r_bot,
+                     height=2*r_bot,
+                     theta1=degrees(dir_between_pts(baseB, baseC)[0]),
+                     theta2=degrees(dir_between_pts(baseB, baseA)[0]),
+                     )
+    ax.add_patch(b)
+    
+    ax.scatter(baseC[0], baseC[1])
+    c = mpatches.Circle(baseC, r_hand, fill=False)
+    ax.add_patch(c)
+    
+    ax.scatter(baseD[0], baseD[1])
+    d = mpatches.Arc(
+                     xy=baseD,
+                     width=2*r_top,
+                     height=2*r_top,
+                     theta1=degrees(dir_between_pts(baseD, baseA)[0]),
+                     theta2=degrees(dir_between_pts(baseD, baseC)[0]),
+                     )
+    ax.add_patch(d)
+    
+    ax.scatter(baseE[0], baseE[1])
+    ax.scatter(baseF[0], baseF[1])
+    ax.scatter(baseG[0], baseG[1])
+    ax.scatter(baseH[0], baseH[1])
+    
+    plt.show()
+
+# Because the shapeoko2 is quite small, the base must be cut in 3 parts which
+#   should be easy to line up.
+# Part0 is for under the left hand plate, includes outline and base holes.
+# Part1 is for the middle where the controller will be mounted.
+# Part2 is for under the right hand plate, includes outline and base holes.
+if base_gcode:
+    pass
